28a9dba71606c1a0750208aebaa1cb078af3536e
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2017-11-13 13:47:13 +0100

    Added undo, keybindings for all keys and exception handling

--- mitalian.py
+++ mitalian.py
@@ -3,30 +3,36 @@ import sys, argparse
 import tkinter as tk
 from os import listdir, rename, makedirs
 from os.path import isfile, join, basename, exists
+import itertools
 from PIL import Image, ImageTk
 
 
 class Window():
     def __init__(self, labels, *, source, dest='.', preload=False):
         assert exists(source), "Source directory does not exist!"
-        if not exists(dest):
-            makedirs(dest_folder)
 
         file_list = [f for f in listdir(source) if isfile(join(source, f))]
         assert len(file_list) > 0, "There are no files in the source directory!"
-
-        self.labels = labels
-        for label in labels.values():
-            if not exists(join(dest, label)):
-                makedirs(join(dest, label))
 
         self.source = source
         self.dest = dest
 
         self.main = tk.Tk()
-        self.main.bind('<Key>', lambda e: self.move_image(e.char) and self.show_next_image())
+        # self.main.bind('<Key>', lambda e: self.move_image(e.char) and self.show_next_image())
         self.main.bind("<Button-1>", self.mouse_callback)
         self.main.bind("<Escape>", lambda x: self.main.destroy())
+        self.main.bind("<BackSpace>", self.undo_action)
+
+        self.labels = {}
+        for label, key in [s.split(':') for s in args.labels]:
+            self.labels[key] = label
+
+            if not exists(join(dest, label)):
+                makedirs(join(dest, label))
+
+            def make_lambda(key):
+                return lambda e: self.move_image(key) and self.show_next_image()
+            self.main.bind(key, make_lambda(key))
 
         # The generator is lazy evalued unless preload is True
         images_gen = (self.load_image(f) for f in file_list)
@@ -35,16 +41,18 @@ class Window():
         else:
             self.images = images_gen
         self.current_image = next(self.images, None)
+        self.previous_images = []
 
         self.build_window()
         self.main.mainloop()
 
     def load_image(self, file):
         tmp = Image.open(join(self.source, file))
+        filename = basename(tmp.filename)
         new_image = ImageTk.PhotoImage(tmp)
-        new_image.filename = basename(tmp.filename)
+        new_image.filename = filename
         return new_image
-        
+
 
     def build_window(self):
         self.canvas = tk.Canvas(self.main, width=500, height=500)
@@ -61,12 +69,18 @@ class Window():
     def mouse_callback(self, event):
         print('mouse called', event)
 
-    def move_image(self, label):
-        print('called', label)
-        prev_filename = self.current_image.filename
-        rename(join(self.source, prev_filename),
-               join(self.dest, self.labels[label], prev_filename))
-        return True
+    def move_image(self, key):
+        print('called', key)
+        try:
+            prev_filename = self.current_image.filename
+            rename(join(self.source, prev_filename),
+                   join(self.dest, self.labels[key], prev_filename))
+
+            self.previous_images.append((self.current_image, key))
+            return True
+        except e:
+            print(e.message)
+            return False
 
 
     def show_next_image(self):
@@ -78,6 +92,17 @@ class Window():
             self.main.destroy()
 
 
+    def undo_action(self, event):
+        if self.previous_images:
+            image, key = self.previous_images.pop(-1)
+            self.images = itertools.chain([image, self.current_image], self.images)
+
+            print(image.filename, key)
+            rename(join(self.dest, self.labels[key], image.filename),
+                   join(self.source, image.filename))
+
+            self.show_next_image()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -86,14 +111,9 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--labels', nargs='+', required=True, help='<Required> List of labels and key. label_name:key')
     parser.add_argument('-p', '--preload', action='store_true', help="Preload all images in memory at the beginning, useful when you have really big images and a lot of memory.")
     args = parser.parse_args()
-    
+
     if len(args.labels) < 2:
         sys.exit("Error: At least two labels are required")
 
-    labels = {}
-    for s in args.labels:
-        label, key = s.split(':')
-        labels[key] = label
+    Window(labels=args.labels, source=args.source, dest=args.dest, preload=args.preload)
 
-    Window(labels, source=args.source, dest=args.dest, preload=args.preload)
-    
